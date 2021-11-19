84818be18a12410bbfc3a4912477ef7a5a586b77
Raghav <bathla.raghav5@gmail.com>
2021-11-19 10:39:22 +0530

    Initial commit

---
+++ src/main/java/com/example/springbootproject/HelloWorldController.java
@@ -0,0 +1,40 @@
+package com.example.springbootproject;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+    @RequestMapping(value={"", "/", "/home"})
+    public String sayHello(){
+        return "Hello Fron Bridgetabz1|1";
+    }
+
+
+    @RequestMapping (value = {"/query"},  method = RequestMethod.GET)
+    public String sayhellow(@RequestParam (value="name") String nane)
+    {
+        return "Hello" + nane +"!";
+
+    }
+    @GetMapping("/param/{name}")
+public String sayHelloParam(@PathVariable String name){
+        return "Hello" + name + "!";
+
+    }
+
+
+    @PostMapping("/post")
+    public String post(@RequestBody User user){
+        return "Hello" + user.getFirstName() + user.getLastName()+"!";
+    }
+
+
+    @PutMapping("/put/{firstname}")
+    public String put(@PathVariable String firstname,@RequestParam(value = "lastname") String lastname)
+    {
+        return "Hello " +firstname+lastname;
+    }
+
+
+}
